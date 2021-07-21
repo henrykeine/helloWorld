cd83363765a5c3c23a477a4e30b4e97dfecb1282
henrykeine <hkeinr22@gmail.com>
2021-07-21 19:11:27 +0530

    first commit

---
+++ matrix.cpp
@@ -0,0 +1,37 @@
+#include<stdio.h>
+int main()
+{
+	int i,j,k,l,first[10][10],second[0][10],sum[10][10];
+	printf("enter no of row and column for first matrix ");
+	scanf("%d%d",&k,&l);
+	printf("enter no of row and column for second matrix");
+	scanf("%d%d",&k,&l);
+	printf("enter the elements of first matrix");
+	for(i=0;i<k;i++)
+	{
+		for(j=0;j<l;j++)
+		{
+			
+		}
+	}
+	scanf("%d",&first[i][j]);
+	printf("enter the elements of second matrix");
+	for(i=0;i<k;i++)
+	{
+		for(j=0;j<l;j++);
+		{
+			
+		}
+	}
+	scanf("%d",&second[i][j]);
+	sum[i][j]=first[i][j]+second[i][j];
+	printf("sum of 2d matrix is");
+	for(i=0;i<k;i++)
+	{
+		for(j=0;j<l;j++)
+		{
+			
+		}
+	}
+	printf("%d",&sum[i][j]);
+	}
